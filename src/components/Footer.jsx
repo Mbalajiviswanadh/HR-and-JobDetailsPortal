f5367edc4f4652a3541ebbfb8e73f7323a0ad797
balajiviswanadh <101652480+Mbalajiviswanadh@users.noreply.github.com>
2024-11-08 12:58:37 +0530

    deployed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,5 @@
+import { FaGithub } from "react-icons/fa";
+
 const Footer = () => {
   return (
     <footer className="bg-gray-200 dark:bg-gray-800 text-center py-4 mt-8">
@@ -11,7 +13,7 @@ const Footer = () => {
           Rohan Dubey
         </a>
       </p>
-      <p className="text-gray-700 dark:text-gray-300 text-sm mt-2">
+      <p className="text-gray-700 dark:text-gray-300 text-sm mt-2 ">
         Created by{" "}
         <a
           href="https://www.linkedin.com/in/balaji-viswanadh-madhavareddy-875473220/"
@@ -19,6 +21,13 @@ const Footer = () => {
           rel="noopener noreferrer"
           className="text-blue-500 hover:text-blue-600 dark:text-blue-400">
           Viswanadh
+        </a>{" "}
+        <a
+          href="https://github.com/Mbalajiviswanadh/HR-and-JobDetailsPortal"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="inline-flex items-center text-gray-700 dark:text-gray-300 hover:text-blue-500 dark:hover:text-blue-400">
+          <FaGithub className="ml-1" />
         </a>
       </p>
     </footer>
